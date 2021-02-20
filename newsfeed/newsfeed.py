9b49fbe3463602dee6c8960b275c310e6cb59d18
Alsrec <47378034+Alsrec@users.noreply.github.com>
2021-02-20 22:39:29 +0800

    Create newsfeed.py

---
+++ newsfeed/newsfeed.py
@@ -0,0 +1,16 @@
+# News feed ingester API(entity)
+
+
+# check_if_exist --> check if the file you want to search is exist
+
+
+
+
+def check_if_exist(filename, status):
+#    if filename does not exist, returen "filename dose not exist"
+#    if the status is not analysed returen "file is not ready for search"
+    return True
+
+def search_on_web(filename, sentiment, keywords)
+    doucument = "...."
+    return doucument
