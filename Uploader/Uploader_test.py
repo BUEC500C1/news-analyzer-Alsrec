e0c9937e9f8d8f207c020c612f574764869b413e
Alsrec <47378034+Alsrec@users.noreply.github.com>
2021-02-20 22:12:50 +0800

    Update Uploader_test.py

--- Uploader/Uploader_test.py
+++ Uploader/Uploader_test.py
@@ -1,19 +1,19 @@
 from uploader import *
 
 def test_create():
-  assert create(1, NULL) == "errorfilename"
+  assert create(1, None) == "errorfilename"
   assert create(1, "hellow") == "success"
   
 def test_Delete():
-  assert Delete(NULL) == "errorfilename"
+  assert Delete(None) == "errorfilename"
   assert Delete({hellow") == "delete file successfully!"
   
 def test_Update():
-  assert Update(NULL) == "errorfilename"
+  assert Update(None) == "errorfilename"
   assert Update("hellow") == "update file successfully"
 
 def test_read():
-  assert read(NULL) == "errorfilename"
+  assert read(None) == "errorfilename"
   assert read("hellow") == "open file succseefully"
   
 
