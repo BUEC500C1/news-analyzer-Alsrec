fefbf26f3f25accca86de3b781046de04bfd9fc9
Alsrec <47378034+Alsrec@users.noreply.github.com>
2021-03-01 11:31:18 +0800

    Update uploader.py

--- Uploader/uploader.py
+++ Uploader/uploader.py
@@ -2,28 +2,39 @@
 
 
 
+import flask
+import json
 import time
+import os
 
-
-
+UPLOAD_DIRECTORY = "/document"
+api = Flask(__name__)
 
 
 # Create--> create a document
 # Delete--> delete a document
 # Update--> update a latest version of a document
 # Read--> look the context of a document
-
-def create(files, filename):
+@api.route("/files/<filename>", methods=["POST"])
+def create(filename):
+    """creat and upload a file"""
     if filename == None:
         print("error filename")
         return "errorfilename"
-    else:
-        name = filename
-        createtime = time.time()
-        print("create file successfully!")
-        return "success"
+    
+    if "/" in filename:
+        # Return 400 BAD REQUEST
+        abort(400, "no subdirectories allowed")
 
+    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
+        fp.write(request.data)
 
+    name = filename
+    createtime = time.time()
+    print("create file successfully!")
+    return "success"
+
+@api.route("/files/<filename>", methods=["DELETE"])
 def delete(filename):
     if filename == None:
         print("error filename")
@@ -31,6 +42,7 @@ def delete(filename):
     else:
         return "delete file successfully!"
 
+@api.route('/update')
 def update(files, filename):
     if filename == None:
         print("error filename")
@@ -38,9 +50,12 @@ def update(files, filename):
     else:
         return "update file successfully"
 
+@api.route('/read')
 def read(filename):
     if filename == None:
         print("error filename")
         return "errorfilename"
     else:
         return "open file succseefully"
+
+
